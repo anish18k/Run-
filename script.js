94b37f349f22ab3e2689295bef9cf4e11cf2144f
anish18k <tjdada837@gmail.com>
2025-09-01 10:12:36 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("touchstart", jump);
-document.addEventListener("keydown", e => { if (e.code === "Space") jump(); });
-document.addEventListener("click", jump);
-document.addEventListener("touchstart", jump); // <--- add this
